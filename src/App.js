b2bf85dc02d5d342a08cdc66b24e4d4b05c350ad
David Armendariz <darmendariz1998@outlook.com>
2020-06-02 11:34:07 -0500

    first commit: added ion button, ionicons and ion toast

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { IonButton, IonToast, IonIcon } from '@ionic/react';
+import { play as playIcon } from 'ionicons/icons';
+
+function App() {
+  const [showToast, setShowToast] = useState(false);
+  const handleClick = () => {
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 1500);
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>My App</h1>
+      </header>
+      <main>
+        <IonButton onClick={handleClick}>
+          <IonIcon icon={playIcon} slot="start" />
+          Click Me
+        </IonButton>
+        <IonToast isOpen={showToast} message="Hello world!" />
+      </main>
+    </div>
+  );
+}
+
+export default App;
